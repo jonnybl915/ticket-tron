979812d8552540395ec6f16442541ecf2f2c4e34
Jon Black <jblack@JolleCocoon.local>
2017-01-24 10:00:17 -0500

    initial import

---
+++ db/migrate/20170124142239_create_issues.rb
@@ -0,0 +1,11 @@
+class CreateIssues < ActiveRecord::Migration
+  def change
+    create_table :issues do |t|
+      t.string :status
+      t.text :description
+      t.string :title
+
+      t.timestamps null: false
+    end
+  end
+end
